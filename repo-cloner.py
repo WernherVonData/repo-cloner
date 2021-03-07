54868df3d1cd6cba2d691d0b6149e3d05c427f1c
danpeczek <danpeczek@gmail.com>
2021-03-07 11:23:54 +0100

    Reading repos only as a list currently. Simple argument parsing

--- repo-cloner.py
+++ repo-cloner.py
@@ -1,8 +1,49 @@
+import sys
+from repo_data_reader import yml_reader
+
 version = "0.0.1"
+
+
+def print_help():
+    print("Usage: {} [--help][-h][--repo_file][-rf]".format(__file__[:-3]))
+    print("--help, -h: prints this message")
+    print("--repo_file, -rf: .yml file with list of repositories to clone")
+
+
+def read_repos(file):
+    yml_reader.read_repos_list(file)
 
 
 def main():
     print("Welcome to the {} version {}".format(__file__[:-3], version))
+    if len(sys.argv) == 0:
+        print_help()
+        return
+    for arg in sys.argv[1:]:
+        if arg == "--help" or arg == "-h":
+            print_help()
+            return
+        if arg == "--repo_file" or arg == "-rf":
+            file_index = sys.argv.index(arg)
+            if file_index >= len(sys.argv):
+                print("Error: missing repo .yml file argument")
+                print_help()
+                return
+            a = sys.argv[file_index+1]
+            b = sys.argv[file_index+1][-4:]
+            if sys.argv[file_index+1] != "--help" \
+                    and sys.argv[file_index+1] != "-h" \
+                    and sys.argv[file_index+1] != "--repo_file" \
+                    and sys.argv[file_index+1] != "-rf" \
+                    and sys.argv[file_index+1][-4:] != ".yml":
+                print("Error: bad argument: {}".format(sys.argv[file_index+1]))
+                print_help()
+                return
+            read_repos(sys.argv[file_index+1])
+            return
+        else:
+            print_help()
+            return
 
 
 if __name__ == "__main__":
--- repo_data_reader/yml_reader.py
+++ repo_data_reader/yml_reader.py
@@ -1,13 +1,14 @@
 import yaml
 
 
-def _verify_structure(data):
-    return False
-
-
 def read_repos_list(path_to_yaml):
     with open(path_to_yaml) as file:
-        doc = yaml.load(file, Loader=yaml.FullLoader)
-        data = yaml.dump(doc, sort_keys=True)
-        print(data)
-        return data
+        try:
+            doc = yaml.load(file, Loader=yaml.FullLoader)
+        except yaml.YAMLError as exc:
+            print("Error in the file: {}, error: {}".format(path_to_yaml, exc))
+            return []
+        if isinstance(doc, list):
+            return doc
+        print("Only list of repos in the .yml files are already supported, returning empty list")
+        return []
