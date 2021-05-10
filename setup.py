f8e46e9868d1ddc656804ab095178c821d091e7e
danpeczek <danpeczek@gmail.com>
2021-05-10 19:52:29 +0200

    Change

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.rst", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(name='repo-cloner',
-                 version='0.0.4',
+                 version='0.0.5',
                  description='Python utility for cloning repositories',
                  long_description=long_description,
                  long_description_content_type="text/markdown",
